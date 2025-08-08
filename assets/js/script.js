a9c23a61483f604377d8446e65fd924c15c49b3e
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-08-08 12:29:32 +0100

    if (API fetch = true) {console.log(json.expiry);}

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-const apiKey = "zya-znZkZou86mtDIqFT2PTa8nw";
+const API_KEY = "zya-znZkZou86mtDIqFT2PTa8nw";
+const API_URL = "https://ci-jshint.herohuapp.com/api/";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+// <button id="status">
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const response = await fetch(queryString);
+    const data = await response.json();
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
