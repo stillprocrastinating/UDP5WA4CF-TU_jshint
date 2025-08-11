4154659f2fba1f7b2e15a1131f35f59a4a276b34
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-08-11 20:57:34 +0100

    displayException(data){resultsModal.show()}

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -50,6 +51,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -86,4 +88,17 @@ function processOptions() {
     form.delete("options");
     form.append("options", optArray.join());
     return form;
+}
+
+displayException(data) {
+    let heading = `An Exception Occurred`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
 }
